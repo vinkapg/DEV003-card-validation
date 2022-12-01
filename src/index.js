61dad91ba5a74c1a16b707256f8c67ddd2a05cf9
vinkapg <vinkapgonzalez@gmail.com>
2022-12-01 14:24:02 -0300

    prueba new

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-import validator from './validator.js';
 
-console.log(validator);
+function validatorCard(){
+    var CardNumber = document.getElementById("cardNumber").value;
+    let arregloTarjeta = [];
+    for (var j= 0; j< CardNumber.length; j++) {
+        
+        arregloTarjeta = parseInt(CardNumber[j]);
+        console.log(arregloTarjeta)
+    }
+    
+}
+
+
