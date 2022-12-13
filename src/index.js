0fbb5cdefe56e52e387feee4da58a626b88717da
vinkapg <vinkapgonzalez@gmail.com>
2022-12-13 14:46:54 -0300

    Cambios semana 3

--- src/index.js
+++ src/index.js
@@ -1,4 +1,89 @@
 import validator from './validator.js';
+//Nombre de la tarjeta
+let casillaNombre = document.querySelector(".card_details_name"); //me permite seleccionar/posicionar el nombre de la tarjeta para trabajar en él
+let asociarInput1 = document.querySelector("#cardName"); //me asocia el id del input en donde se ingresara el nombre
+let errorDiv1 = document.querySelector(".formInput_cardName_error"); //asocia class del error para el input del nameCard
+//.addEventListener -> esto escucha constantemente lo que el usuario ingresa
+    //se agrega un "evento" que se llama input, este se actualiza contantemente, letra por letra.
+asociarInput1.addEventListener("input", ()=>{
+        if(asociarInput1.value == " "){
+            casillaNombre.innerText = "Francisco Urra";
+        }else{
+            casillaNombre.innerText = asociarInput1.value;
+        }
+});
 
+//Número de la tarjeta
+let numeroTarjeta = document.querySelector(".card_number");
+let asociarInput2 = document.querySelector("#cardNumber");
+let errorDiv2 = document.querySelector(".formInput_cardNumber_error");
 
+asociarInput2.addEventListener("input", ()=>{
+    if(asociarInput2.value == " "){
+        numeroTarjeta.innerText = "0000 0000 0000 0000";
+    }else{
+        numeroTarjeta.innerText = asociarInput2.value;
+    }
+});
 
+//Mes de expiración
+let boxMonth = document.querySelector(".card_month");
+let asociarInput3 = document.querySelector("#cardMonth");
+let errorDiv3 = document.querySelector(".formInput_mm_error");
+
+asociarInput3.addEventListener("input", ()=>{
+    if(asociarInput3.value == " "){
+        boxMonth.innerText = "00";
+    }else{
+        boxMonth.innerText = asociarInput3.value;
+    }
+});
+
+//Años de expiración
+let boxYear = document.querySelector(".card_year");
+let asociarInput4 = document.querySelector("#cardYear");
+let errorDiv4 = document.querySelector(".formInput_aa_error");
+
+asociarInput4.addEventListener("input", ()=>{
+    if(asociarInput4.value == " "){
+        boxYear.innerText = "00";
+    }else{
+        boxYear.innerText = asociarInput4.value;
+    }
+});
+
+let boxCvc = document.querySelector(".card_back_cvc");
+let asociarInput5 = document.querySelector("#cardCVC");
+let errorDiv5 = document.querySelector(".formInput_cvc_error");
+
+asociarInput5.addEventListener("input", ()=>{
+    if(asociarInput5.value == " "){
+        boxCvc.innerText = "000";
+    }else{
+        boxCvc.innerText = asociarInput5.value;
+    }
+});
+
+//tomar los números que el usuario ingresa 
+let boxNum = document.querySelector(".card_number");
+let validarNum = document.querySelector("#cardNumber");
+
+// validarNum.addEventListener("elemento", ()=>{
+//     console.log("tengo el valor")
+// })
+
+let datoValidar = validarNum;
+if(Number.isInteger(datoValidar)) {
+  console.log('La variable es entera');
+}
+
+/*al dar clic tome la función del validator.js*/
+// function validatorCard(){
+
+// let cardNumberBox = document.getElementById("cardNumber").value;
+// let boton = document.getElementById("boton");
+
+// boton.addEventListener ("click", function(){
+//     console.log(validator.isValid(cardNumberBox))
+//  });
+// }
--- src/validator.js
+++ src/validator.js
@@ -1,14 +1,51 @@
-const validator = {
-  // ...
-};
+ const validator = {
+     isValid: (numberCard)=>{
+  //Algoritmo de lunh 
+
+ // tomar los numeros en posición impar
+             //tome el string y lo convierta en número, adicional que me de la posición y lo reverse
+ // reverse.parseInt.indexOf(numberCard) 
+//  function digitosNumberCard()
+let longitud = numberCard.lenght //serviría para determinar la longitud del número
+
+ // multiplicar por 2 los números en posición impar
+ 
+ for (let i = 0; i < longitud; i += 2){ //ver si i.lenght = 16 esta bien planteado y si el razonamiento 
+    
+
+
+
+    
+ }
+    
+ // sumar los que tengas mas de 2 digitos para transformarlos en 1 solo digito o restarles 9
+
+ // sumar todas las posiciones del número
+
+ // aplicar módulo de 10 (%) para que me de el residuo de la división 
+ // una vez resuelto se debe dar una condición, si (if) residuo es 0 debe aparecer una alerta 
+ // de numerotarjetavalido y si no (else) debe indicar que el numero de tarjeta es invalido
+ // return fx es dar fin a la función y devolver un valor 
+ if(resultado%10 === 0){
+    return true
+ }else{
+     return false 
+ }
+     },
+    Maskify: ()=>{
+
+    } 
+
+}; // const validator >> es para exportar la información a index.js 
 
 export default validator;
 
-function validatorCard(){
-  var CardNumber = document.getElementById("cardNumber").value;
-  let arregloTarjeta = [];
-  for (var j= 0; j< CardNumber.length; j++) {
-      
-      arregloTarjeta = parseInt(CardNumber[j]);
-      console.log(arregloTarjeta)
-  }}
+ /* //función para separar digitos
+  isValid: function(cardNumber){
+    let arregloTarjeta = [];
+    for (var j= 0; j< cardNumber.length; j++) {
+        
+        arregloTarjeta = parseInt(cardNumber[j]);
+        console.log(arregloTarjeta)
+    }}
+};*/
